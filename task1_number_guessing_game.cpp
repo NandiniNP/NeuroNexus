675c65a6653793c9791351194d53c905c0e9b652
NandiniNP <nandinipoojari484@gmail.com>
2025-04-29 17:47:28 +0530

    Create task1_number_guessing_game.cpp

---
+++ task1_number_guessing_game.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>     
+
+using namespace std;
+
+int main() {
+    srand(time(0)); // Seed random number generator
+    int numberToGuess = rand() % 100 + 1; // Random number between 1 and 100
+    int userGuess;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "Guess a number between 1 and 100:" << endl;
+
+    do {
+        cin >> userGuess;
+        attempts++;
+
+        if (userGuess > numberToGuess) {
+            cout << "Too high! Try again: ";
+        } else if (userGuess < numberToGuess) {
+            cout << "Too low! Try again: ";
+        } else {
+            cout << "Congratulations! You guessed it in " << attempts << " attempts." << endl;
+        }
+
+    } while (userGuess != numberToGuess);
+
+    return 0;
+}
